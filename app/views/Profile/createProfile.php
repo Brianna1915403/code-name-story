d3247fed5b61bb405e1c1c359680862640ce4646
Brianna <56936663+Brianna1915403@users.noreply.github.com>
2021-04-23 17:40:23 -0400

    Updated of the profile creation page.
    Better styling... I think

--- app/views/Profile/createProfile.php
+++ app/views/Profile/createProfile.php
@@ -7,21 +7,21 @@
     </head>
     <body class="light-theme-bg-accent">
         <?= spawnNavBar() ?>
-        <div class="container">
+        <div class="container mb70">
             <div class="hForm card light-theme-bg-main">                
                 <section class="profile-form">
                     <header>
-                        <h2>Create You Profile</h2>
+                        <h2>Create Your Profile!</h2>
                         <p>Let the world know who you are!</p>
                     </header>                
                     <form action="" method="post">
-                        <label>Select a profile picture: <input type="file" name="upload" /></label><br>           
+                        <label>Select a profile picture: <input type="file" name="upload" /></label><br><br />           
                         <label>What your type would you like to be? <select id="account-type" name="account_type">
                             <option value="reader">Reader</option>
                             <option value="writer">Writer</option>
-                        </select></label>
+                        </select></label><br />
                         <div id="account-type-description">As a reader you can keep track of all the stories you love, while showing their creatures love in the comments!</div>
-                        <textarea name="description" cols="50" rows="5" maxlength='255' style='resize: none;'></textarea><br /><br />
+                        <textarea name="description" cols="50" rows="5" maxlength='255' placeholder="Describe yourself!"></textarea><br /><br />
                         <input class='btn light-theme-bg-accent light-theme-text' type="submit" name="action" value="Create Profile">
                     </form>
                 </section>
