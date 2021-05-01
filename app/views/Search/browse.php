bcdf8ef0e75bfa6299a02316438b8e9c26966dd1
SniperPig <alexandrurotaru16@gmail.com>
2021-05-01 15:53:47 -0400

    Added test cards for browse

--- app/views/Search/browse.php
+++ app/views/Search/browse.php
@@ -10,6 +10,241 @@
             if (isset($_GET['tags']))
                 echo $_GET['tags'];
         ?>
+        <div class="container" style="overflow: hidden">
+            <ul class="card_lst" style="">
+            <li>
+                <a href="https://www.webtoons.com/en/action/return-to-player/list?title_no=2574" class="card_item NPI=a:list,i:2574,r:2,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                    <div class="card_flipper">
+                        <div class="card_front">
+                            <img src="https://webtoon-phinf.pstatic.net/20210401_135/1617253261296o2VLt_JPEG/7ReturntoPlayer_desktop_thumbnail.jpg?type=a210" alt="Return to Player" width="210" height="210">
+                            <div class="info">
+                                <h3 class="subj">Return to Player</h3>
+                                <p class="grade_area">
+                                    <span class="ico_like3">like</span><em class="grade_num">274,501</em>
+                                </p>
+                                <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                            </div>
+                            <span class="genre g_action">Action</span>
+                        </div>
+                        
+                        <div class="card_back g_action">
+                            <div class="info">
+                                <h3 class="subj">Return to Player</h3>
+                                <p class="author">UMKY / SEHON</p>
+                                <p class="line"></p>
+                                <p class="summary">Ten years ago, a group of gods turned everyone on Earth into players of a sadistic game that forced them to kill monsters in real life -- or be killed themselves. Sehan Kim has...</p>
+                            </div>
+                        </div>
+                    </div>
+            </a>
+        </li>
+        <li><a href="https://www.webtoons.com/en/fantasy/like-wind-on-a-dry-branch/list?title_no=2405" class="card_item NPI=a:list,i:2405,r:3,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20201212_275/1607741681951KOJfw_JPEG/0LikeWindonaDryBranch_desktop_thumbnail.jpg?type=a210" alt="Like Wind on a Dry Branch" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">Like Wind on a Dry Branch</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">1.6M</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_fantasy">Fantasy</span>
+                    </div>
+                    
+                    <div class="card_back g_fantasy">
+                        <div class="info">
+                            <h3 class="subj">Like Wind on a Dry Branch</h3>
+                            <p class="author">Dalsaeowl / Hwaeum</p>
+                            <p class="line"></p>
+                            <p class="summary">As plague and turmoil afflicts the empire, enchanting widow Rieta Tristi finds herself at the mercy of a malicious nobleman and his dying wi...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/romance/the-kiss-bet/list?title_no=1617" class="card_item NPI=a:list,i:1617,r:4,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20200606_156/1591401716322FNDzo_JPEG/10_EC8DB8EB84A4EC9DBC_ipad.jpg?type=a210" alt="The Kiss Bet" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">The Kiss Bet</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">6.2M</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_romance">Romance</span>
+                    </div>
+                    
+                    <div class="card_back g_romance">
+                        <div class="info">
+                            <h3 class="subj">The Kiss Bet</h3>
+                            <p class="author">Ingrid Ochoa</p>
+                            <p class="line"></p>
+                            <p class="summary">It’s senior year and Sara Lin just turned 18. She’s got great friends, a cool Dad (or so he thinks) and a whole lot ahead of her. The last thing she needs is to worry about having her first kiss. ...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/fantasy/your-throne/list?title_no=2009" class="card_item NPI=a:list,i:2009,r:5,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20200513_90/1589332188875qrVlg_JPEG/10_EC8DB8EB84A4EC9DBC_ipad.jpg?type=a210" alt="Your Throne" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">Your Throne</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">5.9M</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_fantasy">Fantasy</span>
+                    </div>
+                    
+                    <div class="card_back g_fantasy">
+                        <div class="info">
+                            <h3 class="subj">Your Throne</h3>
+                            <p class="author">SAM</p>
+                            <p class="line"></p>
+                            <p class="summary">Tensions are brewing under the seemingly calm surface of the Vasilios Empire, a kingdom ruled by the Imperial Family and the Temple. Lady Medea Solon has lost her place next to ...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/action/the-strongest-florist/list?title_no=2507" class="card_item NPI=a:list,i:2507,r:6,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20210310_235/1615338677293H1SxM_JPEG/8The+Strongest+Florist_desktop_thumbnail.jpg?type=a210" alt="The Strongest Florist" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">The Strongest Florist</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">757,939</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_action">Action</span>
+                    </div>
+                    
+                    <div class="card_back g_action">
+                        <div class="info">
+                            <h3 class="subj">The Strongest Florist</h3>
+                            <p class="author">Hyun hoo Joo / Kumtata</p>
+                            <p class="line"></p>
+                            <p class="summary">Even though he’s built like a fighter with a fearsome scowl, Jaeho would love nothing more than to become a florist instead. Unfortunately, Jaeho’s ove...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/romance/seasonsofblossom/list?title_no=2509" class="card_item NPI=a:list,i:2509,r:7,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20210323_300/16164393854566hlKg_JPEG/0SeasonsOfBlossom_desktop_thumbnail.jpg?type=a210" alt="Seasons of Blossom" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">Seasons of Blossom</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">871,053</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_romance">Romance</span>
+                    </div>
+                    
+                    <div class="card_back g_romance">
+                        <div class="info">
+                            <h3 class="subj">Seasons of Blossom</h3>
+                            <p class="author">HONGDUCK / NEMONE</p>
+                            <p class="line"></p>
+                            <p class="summary">Oh, to be young and in love. Oh, to be wild and carefree. The springtime of life is intense, tumultuous, and at times, oh so confusing. Spring, summer, fall and winter -- love blossoms in ...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/fantasy/winter-moon/list?title_no=1093" class="card_item NPI=a:list,i:1093,r:8,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20210329_255/1617003400467JHgMO_JPEG/4Winter-Moon_ipad.jpg?type=a210" alt="Winter Moon" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">Winter Moon</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">27.6M</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_fantasy">Fantasy</span>
+                    </div>
+                    
+                    <div class="card_back g_fantasy">
+                        <div class="info">
+                            <h3 class="subj">Winter Moon</h3>
+                            <p class="author">Merryweather / MAYUI</p>
+                            <p class="line"></p>
+                            <p class="summary">A gay sorceror and a seductive priestess cross paths in an Online RPG.</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+    
+    
+
+    
+    
+    
+        <li><a href="https://www.webtoons.com/en/romance/death-of-a-pop-star/list?title_no=2273" class="card_item NPI=a:list,i:2273,r:9,g:en_en" data-title-unsuitable-for-children="false" data-title-unsuitable-for-children-skin="harmful_white_skin1">
+                <div class="card_flipper">
+                    <div class="card_front">
+                        <img src="https://webtoon-phinf.pstatic.net/20201014_171/1602613814913xPlvj_JPEG/210_EC8DB8EB84A4EC9DBC_ipad.jpg?type=a210" alt="Death of a Pop Star" width="210" height="210">
+                        <div class="info">
+                            <h3 class="subj">Death of a Pop Star</h3>
+                            <p class="grade_area">
+                                <span class="ico_like3">like</span><em class="grade_num">1.6M</em>
+                            </p>
+                            <p class="icon_area"><span class="txt_ico_up">UP</span></p>
+                        </div>
+                        <span class="genre g_romance">Romance</span>
+                    </div>
+                    
+                    <div class="card_back g_romance">
+                        <div class="info">
+                            <h3 class="subj">Death of a Pop Star</h3>
+                            <p class="author">Violet Karim</p>
+                            <p class="line"></p>
+                            <p class="summary">Number one on the pop charts and in the hearts of fans world-wide, Sophie Lim is at the top of her game -- until she takes a tumble from the top of the stairs. Now she's dead, and...</p>
+                        </div>
+                    </div>
+                </div>
+        </a></li>
+        </ul>
+    </div>
+        
         <?= spawnFooter() ?>
     </body>
 </html>
