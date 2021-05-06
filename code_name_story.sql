ede7e3661c856a14198bf2b0cf76ef565359001f
Brianna <56936663+Brianna1915403@users.noreply.github.com>
2021-05-06 08:54:14 -0400

    Forgot to update the db

--- code_name_story.sql
+++ code_name_story.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2021 at 07:00 PM
+-- Generation Time: May 06, 2021 at 12:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -34,10 +34,24 @@ CREATE TABLE `chapter` (
   `chapter_id` int(11) NOT NULL,
   `story_id` int(11) NOT NULL,
   `chapter_title` varchar(64) NOT NULL,
-  `chapter_text` text NOT NULL,
+  `chapter_text` mediumtext NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
   `likes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chapter`
+--
+
+INSERT INTO `chapter` (`chapter_id`, `story_id`, `chapter_title`, `chapter_text`, `date_created`, `likes`) VALUES
+(1, 1, 'Chapter 1', '6092e4abb09f7.txt', '2021-05-05 18:32:11', 0),
+(2, 1, 'Chapter 2', '6092e4c506196.txt', '2021-05-05 18:32:37', 0),
+(3, 1, 'Chapter 3', '6092e4d9e5ea3.txt', '2021-05-05 18:32:57', 0),
+(4, 1, 'Chapter 4', '6092e4f95ab92.txt', '2021-05-05 18:33:29', 0),
+(5, 2, 'Hairy Happenings', '6093ddb75c049.txt', '2021-05-06 12:14:47', 0),
+(6, 2, 'Wobbeling Whirlwind', '6093ddd3744a6.txt', '2021-05-06 12:15:15', 0),
+(7, 3, 'Chapter 1', '6093e1ce95944.txt', '2021-05-06 12:32:14', 0),
+(8, 4, 'Life in the wasteland [Part 1]', '6093e33a1bf4a.txt', '2021-05-06 12:38:18', 0);
 
 -- --------------------------------------------------------
 
@@ -54,6 +68,21 @@ CREATE TABLE `comment` (
   `date_commented` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `chapter_id`, `profile_id`, `text`, `date_commented`) VALUES
+(1, 1, 2, 'I\'m really looking forward to posting again!!! Hope you all enjoy! :P', '2021-05-06 01:41:08'),
+(6, 3, 3, 'This story is soooooooo hella cool man!', '2021-05-06 12:17:55'),
+(7, 1, 3, 'I am loving every second of it!', '2021-05-06 12:23:37'),
+(8, 6, 5, 'I really, really, really hope that Pigeon saves Troll Garth because I love this story soooooo much!', '2021-05-06 12:40:14'),
+(9, 5, 4, 'A dirty alien, a tribe of girls and a noisy troll - haven\'t we seen this before somewhere?', '2021-05-06 12:41:40'),
+(10, 5, 5, 'Totally know what you mean I just can out a finger on it!', '2021-05-06 12:43:01'),
+(11, 5, 3, 'NoNoNoNONONOnOnONoNonOnOnONONoNoN!', '2021-05-06 12:44:14'),
+(12, 5, 3, 'It\'s a totaly origanl idea I didn\'t get it from anywhere! OK >:(', '2021-05-06 12:45:27'),
+(13, 4, 5, 'I\'ve been waiting all week to read this! made my day!', '2021-05-06 12:46:52');
+
 -- --------------------------------------------------------
 
 --
@@ -66,6 +95,15 @@ CREATE TABLE `favorite_story` (
   `profile_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `favorite_story`
+--
+
+INSERT INTO `favorite_story` (`story_id`, `profile_id`) VALUES
+(1, 3),
+(1, 5),
+(2, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +115,21 @@ CREATE TABLE `liked_chapter` (
   `chapter_id` int(11) NOT NULL,
   `profile_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `liked_chapter`
+--
+
+INSERT INTO `liked_chapter` (`chapter_id`, `profile_id`) VALUES
+(1, 2),
+(1, 3),
+(2, 3),
+(3, 3),
+(4, 5),
+(5, 3),
+(5, 5),
+(6, 3),
+(6, 5);
 
 -- --------------------------------------------------------
 
@@ -98,10 +151,12 @@ CREATE TABLE `picture` (
 --
 
 INSERT INTO `picture` (`picture_id`, `filename`, `alt`, `artist`, `profile_id`) VALUES
-(3, '608d7c3fc5a4f.jpg', 'Jane\'s Profile Picture', '', 1),
-(4, '608dee4388ca1.png', 'E\'s Profile Picture', '', 3),
-(5, '608f2980ca883.jpg', 'Jane\'s Profile Picture', '', 1),
-(6, '608f29bdc5608.jpg', 'Jane\'s Profile Picture', '', 1);
+(1, '6092e48a3e570.jpg', 'Trapped cover', 'Émily Mayodon', 2),
+(2, '6093496c93258.jpg', 'Émily Mayodon\'s Profile Picture', '', 2),
+(3, '6093dda5cbeb6.png', 'Galactic Hairy Gun Wars cover', 'superhelladude99', 3),
+(4, '6093e176e55af.png', 'Two Vile Uncles Jogging to the Beat cover', 'E', 4),
+(5, '6093e2b94d1a3.png', 'Bold Mo Smith cover', 'E', 4),
+(6, '6093e417defba.jpg', 'E\'s Profile Picture', '', 4);
 
 -- --------------------------------------------------------
 
@@ -124,8 +179,10 @@ CREATE TABLE `profile` (
 --
 
 INSERT INTO `profile` (`profile_id`, `user_id`, `account_type`, `description`, `theme`, `profile_picture_id`) VALUES
-(1, 1, 'reader', 'I don\'t know who I am...', 'light', 6),
-(3, 2, 'writer', 'I am a writer!', 'light', 4);
+(2, 2, 'writer', 'I love writing fanfics!', 'light', 2),
+(3, 3, 'writer', 'I\'m kind of a super hella cool dude 99x times over!', 'light', NULL),
+(4, 4, 'writer', 'EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE', 'light', 6),
+(5, 5, 'reader', 'Reading is my only joy in life.......', 'light', NULL);
 
 -- --------------------------------------------------------
 
@@ -139,19 +196,15 @@ CREATE TABLE `reply` (
   `original_comment_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `series`
+-- Dumping data for table `reply`
 --
 
-DROP TABLE IF EXISTS `series`;
-CREATE TABLE `series` (
-  `series_id` int(11) NOT NULL,
-  `profile_id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `reply` (`reply_id`, `original_comment_id`) VALUES
+(7, 1),
+(10, 9),
+(11, 10),
+(12, 9);
 
 -- --------------------------------------------------------
 
@@ -166,7 +219,6 @@ CREATE TABLE `story` (
   `title` varchar(64) NOT NULL,
   `description` text NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `series_id` int(11) DEFAULT NULL,
   `author` varchar(64) NOT NULL,
   `story_picture_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -175,8 +227,11 @@ CREATE TABLE `story` (
 -- Dumping data for table `story`
 --
 
-INSERT INTO `story` (`story_id`, `profile_id`, `title`, `description`, `date_created`, `series_id`, `author`, `story_picture_id`) VALUES
-(1, 1, 'Trapped', 'Eren Jaeger lived in a world of fear, until he woke up in a cramped room with a bunch of strangers. Confused, he asked what was going on, but no one seemed to know the answer. Where is he? Who are they? And why aren\'t they fearing the worst?', '2021-05-01 13:55:01', NULL, 'Émilie Mayodon', 5);
+INSERT INTO `story` (`story_id`, `profile_id`, `title`, `description`, `date_created`, `author`, `story_picture_id`) VALUES
+(1, 2, 'Trapped', 'Eren Jaeger lived in a world of fear, until he woke up in a cramped room with a bunch of strangers. Confused, he asked what was going on, but no one seemed to know the answer. Where is he? Who are they? And why aren\'t they fearing the worst?', '2021-05-05 18:31:38', 'Émily Mayodon', 1),
+(2, 3, 'Galactic Hairy Gun Wars', 'What happens when the war turns hairy.', '2021-05-06 12:14:29', 'superhelladude99', 3),
+(3, 4, 'Two Vile Uncles Jogging to the Beat', 'When exercise gets a little to crazy, who else would be the cause but you vile uncle Tony!', '2021-05-06 12:30:46', 'E', 4),
+(4, 4, 'Bold Mo Smith', 'No one is bolder than Mo Smith, but how bold can you be when life trows everything you way!', '2021-05-06 12:36:09', 'E', 5);
 
 -- --------------------------------------------------------
 
@@ -195,7 +250,15 @@ CREATE TABLE `story_tags` (
 --
 
 INSERT INTO `story_tags` (`tag_id`, `story_id`) VALUES
+(1, 2),
+(5, 4),
+(7, 4),
 (12, 1),
+(12, 3),
+(16, 4),
+(19, 2),
+(22, 3),
+(25, 3),
 (27, 1);
 
 -- --------------------------------------------------------
@@ -262,8 +325,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password_hash`, `token`) VALUES
-(1, 'Jane', '$2y$10$mWSc/wW9YDMcbaHuWRxaDeMjb0LXgj9B2BdPdqiXOjrwUEdcoQxOe', ''),
-(2, 'E', '$2y$10$VeyYaIypjvJHyojSl11C1upN5Fv1YWCNXhMMB28jQDLC967amC4Hi', '');
+(2, 'Émily Mayodon', '$2y$10$P.1tAHWVu9FGY6jYNfGIU.eRaQbECyJ81c2dPFEHcMrQC5ucdhO2y', ''),
+(3, 'superhelladude99', '$2y$10$gCOckkoz7lOFoSbP1sh6yOfifMZ6QGUR1ry2mKQLUOFQBYstBvaWu', ''),
+(4, 'E', '$2y$10$ujw5CCxsSntwi6KT3EJ1mutMezsWcL0UMgpihbss2KrQXAZACihVW', ''),
+(5, 'Jane', '$2y$10$na5ZvVy6UZaotChA3qRumu6mY7.EU65HkMifnW/NQNeOLLU9zA7de', '');
 
 --
 -- Indexes for dumped tables
@@ -322,20 +387,12 @@ ALTER TABLE `reply`
   ADD KEY `reply-original_comment_FK` (`original_comment_id`);
 
 --
--- Indexes for table `series`
---
-ALTER TABLE `series`
-  ADD PRIMARY KEY (`series_id`),
-  ADD KEY `Series_Profile_FK` (`profile_id`);
-
---
 -- Indexes for table `story`
 --
 ALTER TABLE `story`
   ADD PRIMARY KEY (`story_id`,`profile_id`),
   ADD KEY `story_profile_FK` (`profile_id`),
-  ADD KEY `story_picture_FK` (`story_picture_id`),
-  ADD KEY `story_series_FK` (`series_id`);
+  ADD KEY `story_picture_FK` (`story_picture_id`);
 
 --
 -- Indexes for table `story_tags`
@@ -364,13 +421,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `chapter`
 --
 ALTER TABLE `chapter`
-  MODIFY `chapter_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `chapter_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `picture`
@@ -382,31 +439,25 @@ ALTER TABLE `picture`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `series`
---
-ALTER TABLE `series`
-  MODIFY `series_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `story`
 --
 ALTER TABLE `story`
-  MODIFY `story_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `story_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tag`
 --
 ALTER TABLE `tag`
-  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -416,21 +467,21 @@ ALTER TABLE `user`
 -- Constraints for table `chapter`
 --
 ALTER TABLE `chapter`
-  ADD CONSTRAINT `chapter_story_FK` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`);
+  ADD CONSTRAINT `chapter_story_FK` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_chapter_FK` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`chapter_id`),
-  ADD CONSTRAINT `comment_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+  ADD CONSTRAINT `comment_chapter_FK` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`chapter_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `favorite_story`
 --
 ALTER TABLE `favorite_story`
-  ADD CONSTRAINT `fav-story_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`),
-  ADD CONSTRAINT `fav-story_story_FK` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`);
+  ADD CONSTRAINT `fav-story_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fav-story_story_FK` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `liked_chapter`
@@ -456,22 +507,15 @@ ALTER TABLE `profile`
 -- Constraints for table `reply`
 --
 ALTER TABLE `reply`
-  ADD CONSTRAINT `reply-original_comment_FK` FOREIGN KEY (`original_comment_id`) REFERENCES `comment` (`comment_id`),
-  ADD CONSTRAINT `reply_comment_FK` FOREIGN KEY (`reply_id`) REFERENCES `comment` (`comment_id`);
-
---
--- Constraints for table `series`
---
-ALTER TABLE `series`
-  ADD CONSTRAINT `Series_Profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+  ADD CONSTRAINT `reply-original_comment_FK` FOREIGN KEY (`original_comment_id`) REFERENCES `comment` (`comment_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reply_comment_FK` FOREIGN KEY (`reply_id`) REFERENCES `comment` (`comment_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `story`
 --
 ALTER TABLE `story`
   ADD CONSTRAINT `story_picture_FK` FOREIGN KEY (`story_picture_id`) REFERENCES `picture` (`picture_id`),
-  ADD CONSTRAINT `story_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`),
-  ADD CONSTRAINT `story_series_FK` FOREIGN KEY (`series_id`) REFERENCES `series` (`series_id`);
+  ADD CONSTRAINT `story_profile_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
 
 --
 -- Constraints for table `story_tags`
